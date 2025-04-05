fbc3bdaa89df091cbda1af0c8a6bcc236d4ecf73
Nikolaj Bjorner <nbjorner@microsoft.com>
2025-04-05 11:19:07 -0700

    Add Windows-specific signal handling in scoped_ctrl_c

--- src/util/scoped_ctrl_c.cpp
+++ src/util/scoped_ctrl_c.cpp
@@ -17,8 +17,50 @@ Author:
 Revision History:
 
 --*/
+#ifdef _WINDOWS
+#include<signal.h>
+#include "util/scoped_ctrl_c.h"
+static scoped_ctrl_c * g_obj = nullptr;
+
+static void on_ctrl_c(int) {
+    if (g_obj->m_first) {
+        g_obj->m_cancel_eh(CTRL_C_EH_CALLER);
+        if (g_obj->m_once) {
+            g_obj->m_first = false;
+            signal(SIGINT, on_ctrl_c); // re-install the handler
+        }
+    }
+    else {
+        signal(SIGINT, g_obj->m_old_handler); 
+        raise(SIGINT);
+    }
+}
+
+scoped_ctrl_c::scoped_ctrl_c(event_handler & eh, bool once, bool enabled):
+    m_cancel_eh(eh), 
+    m_first(true),
+    m_once(once),
+    m_enabled(enabled),
+    m_old_scoped_ctrl_c(g_obj) {
+    if (m_enabled) {
+        g_obj = this;
+        m_old_handler = signal(SIGINT, on_ctrl_c); 
+    }
+}
+
+scoped_ctrl_c::~scoped_ctrl_c() { 
+    if (m_enabled) {
+        g_obj = m_old_scoped_ctrl_c;
+        if (m_old_handler != SIG_ERR) {
+            signal(SIGINT, m_old_handler);
+        }
+    }
+}
+
+#else
 #include <signal.h>
 #include <cstring>
+#include <mutex>
 #include "util/gparams.h"
 #include "util/scoped_ctrl_c.h"
 
@@ -108,3 +150,4 @@ found:
         signal_unlock();
     }
 }
+#endif
